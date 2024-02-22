dcafe19d2cf6096e02ee3bbc3e95cda27350b0a5
UrosTim <urostimapps@gmail.com>
2024-02-22 22:15:17 +0100

    initial commit

---
+++ sudoku.py
@@ -0,0 +1,71 @@
+board = [
+    [9, 6, 0, 4, 0, 0, 0, 0, 8],
+    [1, 0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 5, 0, 0],
+    [0, 0, 0, 6, 0, 0, 0, 0, 4],
+    [6, 0, 0, 8, 0, 0, 2, 0, 0],
+    [0, 0, 7, 0, 0, 0, 0, 0, 3],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [7, 5, 3, 0, 0, 0, 1, 0, 0],
+    [2, 1, 0, 0, 9, 6, 0, 0, 0]
+]
+
+def print_board(board):
+    for i in range(len(board)):
+        if i != 0 and i % 3 == 0:
+            print("- - - - - - - - - - -")
+        for j in range(len(board[0])):
+            if j != 0 and j % 3 == 0:
+                print("| ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+            
+def valid(board, number, position):
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+        
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+        
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+            
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0
+
+    return False
+
+print_board(board)
+solve(board)
+print("--------------------------------------------------------------------")
+print_board(board)
